1788164d0c7dc44a3a3745f675c1f7a1c46a88c9
avima <avima@dtu.dk>
2022-06-21 11:16:03 +0200

    Fixed secondary structure elements.

--- 3_Extract_protein_multi.py
+++ 3_Extract_protein_multi.py
@@ -255,7 +255,7 @@ def get_resi_id(resi):
 
 def get_SS(dssp_dict, chain_id, resi):
     
-    func_var = dssp_dict[chain_id , (' ', resi.get_id()[1], resi.get_id()[2])][1]
+    func_var = dssp_dict[chain_id , (' ', resi.get_id()[1], resi.get_id()[2])][2]
     
     return func_var
 
@@ -273,7 +273,7 @@ def get_resi_ASA(dssp_dict, chain_id, resi):
     
     # Function calculates available surface area of the residue using the dssp
     
-    func_var = dssp_dict[chain_id , (' ', resi.get_id()[1], resi.get_id()[2])][2]
+    func_var = dssp_dict[chain_id , (' ', resi.get_id()[1], resi.get_id()[2])][3]
     
     return func_var
 
